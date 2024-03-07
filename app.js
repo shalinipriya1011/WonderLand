3dac85a37543e0a313621d963ff63a6027888a76
shalinipriya1011 <70254428+shalinipriya1011@users.noreply.github.com>
2024-03-07 23:14:40 +0530

    updates

--- app.js
+++ app.js
@@ -1,82 +1,88 @@
 const express = require("express");
-const app = express();
+const app=express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-const path = require("path");
+const path=require("path");
 const methodOverride = require("method-override");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
+const MONGO_URL = "mongodb://127.0.0.1:27017/WONDERLAND";
+main().then(() =>{
+console.log("connected to DB");
+}).catch(err => {
     console.log(err);
-  });
-
-async function main() {
-  await mongoose.connect(MONGO_URL);
+});
+async function main()
+{
+   await mongoose.connect(MONGO_URL);
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.get("/",(req,res) => {
+res.send("hi I am root");
 });
 
-//Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-});
+app.get("/listings",async (req,res) =>{
+   const allListings = await Listing.find({});
+   res.render("listings/index.ejs",{allListings});
+    });
+// app.get("/testListing", async(req,res) =>{
+//     let sampleListing = new Listing({
+//         title: "My new journey",
+//         description:" By the beach",
+//         price: 100000,
+//         location: "lakshadweep",
+//         country:"India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
+
 
 //New Route
 app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
+    res.render("listings/new.ejs");
+  });
 
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+//show route
+app.get("/listings/:id", async(req,res) => {
+    let {id} = req.params;
+   const listing= await Listing.findById(id);
+   res.render("listings/show.ejs",{listing});
+})
 
 //Create Route
 app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  });
 
-//Edit Route
+  //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
-
-//Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
-
-//Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
-
-
-
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  });
+  
+  //Update Route
+  app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  });
+  
+  //Delete Route
+  app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  });
+app.listen(8080,() => {
+    console.log("server is listening to port 8080");
 });
